3071fb9db04913c60ec7423e0f4e5f2fbe974ed3
FF <wjdrldud98@gmail.com>
2025-05-07 15:48:38 +0900

    Feat: evaluateTokens 결과값 계산 함수 추가

---
+++ evaluator.js
@@ -0,0 +1,58 @@
+const { performOperation } = require("./operators");
+
+function calculateHighPriority(tokens) {
+  const result = [];
+  result.push(tokens[0]);
+  for (let i = 1; i < tokens.length; i += 2) {
+    const operator = tokens[i];
+    const nextNumber = tokens[i + 1];
+    if (operator === "x" || operator === "/") {
+      const prevNumber = result.pop();
+      const calculatedValue = performOperation(
+        prevNumber,
+        operator,
+        nextNumber
+      );
+      result.push(calculatedValue);
+    } else {
+      result.push(operator);
+      result.push(nextNumber);
+    }
+  }
+
+  return result;
+}
+
+function calculateLowPriority(tokens) {
+  let result = tokens[0];
+  for (let i = 1; i < tokens.length; i += 2) {
+    const operator = tokens[i];
+    const nextNumber = tokens[i + 1];
+    result = performOperation(result, operator, nextNumber);
+  }
+
+  return result;
+}
+
+function evaluateTokens(tokens) {
+  if (tokens.length === 0) {
+    throw new Error("토큰이 비어있습니다.");
+  }
+  for (let i = 0; i < tokens.length; i++) {
+    if (i % 2 === 0 && typeof tokens[i] !== "number") {
+      throw new Error(
+        "토큰 형식이 올바르지 않습니다: 짝수 인덱스는 숫자여야 합니다."
+      );
+    }
+    if (i % 2 === 1 && typeof tokens[i] !== "string") {
+      throw new Error(
+        "토큰 형식이 올바르지 않습니다: 홀수 인덱스는 연산자여야 합니다."
+      );
+    }
+  }
+
+  const highPriorityResult = calculateHighPriority(tokens);
+  return calculateLowPriority(highPriorityResult);
+}
+
+module.exports = { evaluateTokens };
---
+++ evaluator.test.js
@@ -0,0 +1,20 @@
+const { evaluateTokens } = require("./evaluator.js");
+
+describe("evaluateTokens Test", () => {
+  test("[]", () => {
+    expect(() => evaluateTokens([])).toThrow("토큰이 비어있습니다.");
+  });
+  test("[2, +, -, +, 2]", () => {
+    expect(() => evaluateTokens([2, "+", "-", "+", 2])).toThrow(
+      "토큰 형식이 올바르지 않습니다: 짝수 인덱스는 숫자여야 합니다."
+    );
+  });
+  test("[2, 3, 4]", () => {
+    expect(() => evaluateTokens([2, 3, 4])).toThrow(
+      "토큰 형식이 올바르지 않습니다: 홀수 인덱스는 연산자여야 합니다."
+    );
+  });
+  test("[3.5, '+'', 4, 'x', 16]", () => {
+    expect(evaluateTokens([3.5, "+", 4, "x", 16])).toBe(67.5);
+  });
+});
