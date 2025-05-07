e9c0f1a947d838b53e4beaa035bc04d2d2cfa934
FF <wjdrldud98@gmail.com>
2025-05-07 15:47:22 +0900

    Feat: performOperation 기본 연산 함수 추가

---
+++ operators.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function performOperation(a, operator, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      throw new Error("연산자가 입력되어야 합니다.");
+  }
+}
+
+module.exports = { performOperation };
